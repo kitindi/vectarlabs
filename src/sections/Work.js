281c3d9d71223c76fb58bed61d792bed5387fce9
kitindi <abdullkitindi@gmail.com>
2023-12-01 16:16:29 +0300

    changed profiles opacity

--- src/sections/Work.js
+++ src/sections/Work.js
@@ -11,20 +11,20 @@ const Work = () => {
       </div>
       <div className="w-full flex flex-col sm:grid sm:grid-cols-2 gap-3 pX-8 py-16">
         <div className="relative">
-          <div className="absolute top-0 left-0 w-full z-10 bg-[#101117] opacity-90 h-full hover:opacity-0 transition-opacity ease-out cursor-pointer"></div>
+          <div className="absolute top-0 left-0 w-full z-10 bg-[#101117] opacity-70 h-full hover:opacity-0 transition-opacity ease-out cursor-pointer"></div>
           <img src={learnly} alt="" className="h-full" />
         </div>
         <div className="relative">
-          <div className="absolute top-0 left-0 w-full z-10 bg-[#101117] opacity-90 h-full hover:opacity-0 transition-opacity ease-out cursor-pointer"></div>{" "}
+          <div className="absolute top-0 left-0 w-full z-10 bg-[#101117] opacity-70 h-full hover:opacity-0 transition-opacity ease-out cursor-pointer"></div>{" "}
           <img src={softfy} alt="" className="h-full" />
         </div>
 
         <div className="relative">
-          <div className="absolute top-0 left-0 w-full z-10 bg-[#101117] opacity-90 h-full hover:opacity-0 transition-opacity ease-out cursor-pointer"></div>{" "}
+          <div className="absolute top-0 left-0 w-full z-10 bg-[#101117] opacity-70 h-full hover:opacity-0 transition-opacity ease-out cursor-pointer"></div>{" "}
           <img src={learnly} alt="" className="h-full" />
         </div>
         <div className="relative">
-          <div className="absolute top-0 left-0 w-full z-10 bg-[#101117] opacity-90 h-full hover:opacity-10 transition-opacity transition-duration: 150ms easy-out cursor-pointer"></div>{" "}
+          <div className="absolute top-0 left-0 w-full z-10 bg-[#101117] opacity-70 h-full hover:opacity-10 transition-opacity transition-duration: 150ms easy-out cursor-pointer"></div>{" "}
           <img src={softfy} alt="" className="h-full" />
         </div>
       </div>
